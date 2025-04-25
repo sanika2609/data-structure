f8a6f0c8ee79f31cff64f42bfa7b37524d7a44d0
sanika2609 <159999795+sanika2609@users.noreply.github.com>
2025-04-25 23:08:15 +0530

    Add files via upload

---
+++ largest_element_index.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int nums[]={10,24,25,15,34,-12};
+    int size=6;
+     //int INT_MAX;
+     int INT_MIN;
+   // int smallest=INT_MAX;
+    int largest=INT_MIN;
+    for(int i=0;i<size;i++)
+    {
+       // smallest= min (nums[i],smallest);
+        largest= max (i,largest);
+    }
+    
+    //cout<<"smallest:"<<smallest<<endl;
+    cout<<"largest:"<<largest;
+    
+    return 0;
+}
